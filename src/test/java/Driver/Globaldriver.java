12531146b1423f7a335d2a049ea5b4915333773f
Anas Sohail <anas.sohail@venturedive.com>
2023-12-18 17:32:40 +0500

    2nd Commit own framework Tasks

--- src/test/java/Driver/Globaldriver.java
+++ src/test/java/Driver/Globaldriver.java
@@ -1,8 +1,11 @@
 package Driver;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
@@ -10,18 +13,25 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Globaldriver {
-    @Test
-    public static void StartWebDriver() throws IOException {
+    public static WebDriver StartWebDriver() throws IOException {
         Properties prop = new Properties();
         FileInputStream ip = new FileInputStream("C:\\Users\\anas.sohail_ventured\\Desktop\\Selenium-Project-main\\src\\test\\java\\Resources\\config.properties");
         prop.load(ip);
+        WebDriver driver;
         if(prop.getProperty("browser").equals("chrome")){
-            WebDriver driver = new ChromeDriver();
+            ChromeOptions op = new ChromeOptions();
+            op.addArguments("--start-maximized");
+            op.addArguments("start-fullscreen");
+            driver = new ChromeDriver(op);
             driver.navigate().to(prop.getProperty("url"));
         }
         else{
-            WebDriver driver = new FirefoxDriver();
+            FirefoxOptions op = new FirefoxOptions();
+            op.addArguments("--start-maximized");
+            op.addArguments("start-fullscreen");
+            driver = new FirefoxDriver(op);
             driver.navigate().to(prop.getProperty("url"));
         }
+        return driver;
     }
 }
--- src/test/java/Task3/Scenario2.java
+++ src/test/java/Task3/Scenario2.java
@@ -1,5 +1,6 @@
 package Task3;
 
+import Driver.Globaldriver;
 import Elements.Web_elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,23 +16,11 @@ import java.util.Properties;
 public class Scenario2 {
     @Test
     public void login_valid() throws IOException {
-        Properties prop = new Properties();
-        FileInputStream ip = new FileInputStream("C:\\Users\\anas.sohail_ventured\\Desktop\\Selenium-Project-main\\src\\test\\java\\Resources\\config.properties");
-        prop.load(ip);
-        if(prop.getProperty("browser").equals("chrome")){
-            WebDriver driver = new ChromeDriver();
-            driver.navigate().to(prop.getProperty("url"));
-            Web_elements.username(driver).sendKeys("standard_user");
-            Web_elements.password(driver).sendKeys("secret_sauce");
-            Web_elements.login_btn(driver).click();
-        }
-        if(prop.getProperty("browser").equals("firefox")){
-            WebDriver driver2 = new FirefoxDriver();
-            driver2.navigate().to(prop.getProperty("url"));
-            Web_elements.username(driver2).sendKeys("standard_user");
-            Web_elements.password(driver2).sendKeys("secret_sauce");
-            Web_elements.login_btn(driver2).click();
-        }
+        WebDriver driver = Globaldriver.StartWebDriver();
+        Web_elements.username(driver).sendKeys("standard_user");
+        Web_elements.password(driver).sendKeys("secret_sauce");
+        Web_elements.login_btn(driver).click();
+        driver.quit();
     }
 
 }
