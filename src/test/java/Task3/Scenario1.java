e8fc9367e82218b0b6c7fae65ea9dd03156f07ac
Anas Sohail <anas.sohail@venturedive.com>
2024-08-15 10:46:06 +0500

    Updates in percy code

--- src/test/java/Task3/Scenario1.java
+++ src/test/java/Task3/Scenario1.java
@@ -25,20 +25,24 @@ public class Scenario1 {
             WebDriver driver = new ChromeDriver();
             percy = new Percy(driver);
             driver.navigate().to(prop.getProperty("url"));
+            //percy.screenshot("Screenshot_1");
             percy.snapshot("Screenshot_1");
             Web_elements.username(driver).sendKeys("standard_user");
             Web_elements.password(driver).sendKeys("secret123");
             Web_elements.login_btn(driver).click();
+            //percy.screenshot("Screenshot_2");
             percy.snapshot("Screenshot_2");
         }
         if(prop.getProperty("browser").equals("firefox")){
             WebDriver driver2 = new FirefoxDriver();
             percy = new Percy(driver2);
             driver2.navigate().to(prop.getProperty("url"));
+            //percy.screenshot("Screenshot_1");
             percy.snapshot("Screenshot_1");
             Web_elements.username(driver2).sendKeys("standard_user");
             Web_elements.password(driver2).sendKeys("secret123");
             Web_elements.login_btn(driver2).click();
+            //percy.screenshot("Screenshot_2");
             percy.snapshot("Screenshot_2");
         }
     }
--- src/test/java/Task3/Scenario2.java
+++ src/test/java/Task3/Scenario2.java
@@ -26,20 +26,24 @@ public class Scenario2 {
             percy = new Percy(driver);
             driver.navigate().to(prop.getProperty("url"));
             percy.snapshot("Screenshot_1");
+            //percy.screenshot("Screenshot_1");
             Web_elements.username(driver).sendKeys("standard_user");
             Web_elements.password(driver).sendKeys("secret_sauce");
             Web_elements.login_btn(driver).click();
-            percy.snapshot("Screenshot_1");
+            percy.snapshot("Screenshot_2");
+            //percy.screenshot("Screenshot_2");
         }
         if(prop.getProperty("browser").equals("firefox")){
             WebDriver driver2 = new FirefoxDriver();
             percy = new Percy(driver2);
             driver2.navigate().to(prop.getProperty("url"));
             percy.snapshot("Screenshot_1");
+            //percy.screenshot("Screenshot_1");
             Web_elements.username(driver2).sendKeys("standard_user");
             Web_elements.password(driver2).sendKeys("secret_sauce");
             Web_elements.login_btn(driver2).click();
             percy.snapshot("Screenshot_1");
+            //percy.screenshot("Screenshot_2");
         }
     }
 
